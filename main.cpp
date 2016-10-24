a0215d02c311401a065be02350755ef5bf0996af
Ian Blaise <ian.blaise2346@email.bakersfieldcollege.edu>
2016-10-23 18:24:01 -0700

    Update main.cpp
    Added a bit more command handling. 
    Can now create new files.

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace std;
 
 //Function Prototypes for menu.
 void decisionHandler(int);
-
-//Function Prototypes for initilization
-void openFile(string);
-void createFile(string);
-void readFile();
-void getFileName();
+void showOptions();
 
 //Function Prototypes for file editing.
 void substitute(string, string);
@@ -21,92 +15,118 @@ void type(int);
 void copy(int);
 void paste(void);
 void locate(string);
-void insert(int);
+void insert();
 void deleteLines(int);
 void replace(int);
-void move(int);
+void moveCurrentLine(int);
 void quit(void);
 
 //Global Variables
-int CurrentLine = 0;
-string CopiedString = "";
-string FileName = "";
-ifstream FileStream;
+int currentLine = 0;
+ifstream fileIn;
+ofstream fileOut;
+vector<string> fileData;
 
 int main()
 {
-	int choice = 0;
-	cout << "Line Editor\n1)Open Existing\n2)Create New\n3)Exit\nEnter your choice: ";
-	cin >> choice;
-	if(!isdigit(choice))
+	while (true) 
 	{
-		cout << "Invalid choice. Not a number. " << endl;
-		return -1;
-	}
-	while(choice > 3 || choice < 1)
-	{
-		cout << "invalid choice. try again.\nEnter your choice: ";
+		system("CLS");
+		int choice = 0;
+		cout << "Line Editor\n1)Open Existing\n2)Create New\n3)Exit\nEnter your choice: ";
 		cin >> choice;
+		cin.ignore();
+		while (choice > 3 || choice < 1)
+		{
+			cout << "invalid choice. try again.\nEnter your choice: ";
+			cin >> choice;
+			cin.ignore();
+		}
+
+		decisionHandler(choice);
 	}
-
-	decisionHandler(choice);
-
 	return 0;
 }
 
-/* Sets the variable FileName to a user specified value. */
-void getFileName()
+void openExisting()
 {
-	string fileName = "";
-	cout << "Enter the file name: ";
-	cin >> fileName;
-	
-	FileName = fileName;
-}
-/* Creates a new file with a name specified by the user in the default directory. */
-void createFile(string fileName)
-{
-
-}
-
-/* Opens a file specified by the user. */
-void openFile(string fileName)
-{
-	FileStream.open(fileName);
-	if(!FileStream.is_open())
-		cout << "Failed to open the file, does it exist? (" << fileName << ")" << endl;
-	FileName = fileName;
-	readFile();
-}
-
-/* Reads the file data, stores all lines into an array. */
-void readFile()
-{
-	string line = "";
-	if(!FileStream.is_open())
-		cout << "The file stream is closed, no data to read." << endl;
-	while(getline(FileStream,line))
+	cout << "Enter the file's path: ";
+	string filePath;
+	getline(cin, filePath);
+	fileIn.open(filePath);
+	if (!fileIn.is_open())
 	{
-		cout << line << endl;
+		cout << "Failed to open the file! (" << filePath << ")" << endl;
+		return;
+	}
+	else
+	{
+		cout << "File opened successfully!" << endl;
+		showOptions();
+	}
+}
+
+void createNew()
+{
+	cout << "Enter the name of the file to create: ";
+	string fileName;
+	getline(cin, fileName);
+	fileOut.open(fileName);
+	if (fileOut.is_open())
+	{
+		cout << "created " << fileName << " successfully.";
+		fileOut.close();
+	}
+	else
+	{
+		cout << "failed to create " << fileName << ". " << endl;
+	}
+}
+
+void showOptions()
+{
+	bool waiting = true;
+	while (waiting)
+	{
+		char cmd = ' ';
+		cout << "Enter a command (I <#>/T <#>/M <#>): ";
+		cin >> cmd;
+		tolower(cmd);
+
+		switch (cmd)
+		{
+			case 'i':
+				insert();
+				break;
+			case 't':
+				type(1);
+				break;
+			case 'm':
+				move(1);
+				break;
+			case 'q':
+				waiting = false;
+				break;
+			default:
+				cout << "Unknown command!" << endl;
+				break;
+		}
 	}
 }
 
 /* Determines what operation to perform next, based upon user's input. */
 void decisionHandler(int choice)
 {
-	switch(choice)
+	switch (choice)
 	{
 		case 1:
-			cout << "Case 1." << endl; // This will open existing file.
-			getFileName();
-			openFile(FileName);
-			readFile();
+			openExisting(); //Opens existing file.. starts listening for cmd input.
 			break;
 		case 2:
-			cout << "Case 2." << endl; // This will create a new file.
+			createNew(); //Creates new file.. starts listening for cmd input.
 			break;
 		case 3:
-			cout << "Case 3." << endl; // This will exit.
+			quit();
 			break;
 		default:
 			cout << "Unexpected choice. (" << choice << ")." << endl; // This should never be executed but jst in case.
@@ -114,61 +134,70 @@ void decisionHandler(int choice)
 	}
 }
 
-/* Substitute newstring for every occurrence of oldstring in the current line. 
- * Issue a message if oldstring does not appear in the current line. Print the changed line. */
+/* Substitute newstring for every occurrence of oldstring in the current line.
+* Issue a message if oldstring does not appear in the current line. Print the changed line. */
 void substitute(string oldString, string newString)
 {
+	cout << "Substitute called!" << endl;
 }
 
 /* Print the contents of the next # lines, including the current one.
- * Don�t print blank spaces at the end of a line. 
- * The current line should be the last typed. */
+* Don�t print blank spaces at the end of a line.
+* The current line should be the last typed. */
 void type(int nextLines)
 {
+	cout << "Type called!" << endl;
 }
 
-/* Copy the next # lines, including the current one, to a temporary storage area. 
- * The current line should not change. */
+/* Copy the next # lines, including the current one, to a temporary storage area.
+* The current line should not change. */
 void copy(int nextLines)
 {
+	cout << "Copy called!" << endl;
 }
 
-/* Copy the contents of the temporary storage area to a position between the 
- * current line and the line following the current line. 
- * The current line should become the last line pasted. */
+/* Copy the contents of the temporary storage area to a position between the
+* current line and the line following the current line.
+* The current line should become the last line pasted. */
 void paste()
 {
+	cout << "Paste called!" << endl;
 }
 
-/* Find the next occurrence of the string and make the line containing it the 
- * current line. If string does not occur anywhere in the file after the current line, 
- * issue a message and don�t change the current line. Then print the current line. */
+/* Find the next occurrence of the string and make the line containing it the
+* current line. If string does not occur anywhere in the file after the current line,
+* issue a message and don�t change the current line. Then print the current line. */
 void locate(string searchString)
 {
+	cout << "Locate called!" << endl;
 }
 
-/* Insert # lines into the file following the current line. 
- * The current line should be the last line entered. */
-void insert(int nextLines)
+/* Insert # lines into the file following the current line.
+* The current line should be the last line entered. */
+void insert()
 {
+	cout << "Insert called!" << endl;
 }
 
-/* Delete the next # lines, including the current one. 
- * Make the first line follwoing the deleted section the current line. */
+/* Delete the next # lines, including the current one.
+* Make the first line follwoing the deleted section the current line. */
 void deleteLines(int nextLines)
 {
+	cout << "Delete called!" << endl;
 }
 
-/* Replace the next # lines, beginning with the current one, with # lines. 
- * The current line should be the last line entered. */
+/* Replace the next # lines, beginning with the current one, with # lines.
+* The current line should be the last line entered. */
 void replace(int nextLines)
 {
+	cout << "Replace called!" << endl;
 }
 
 /* Locate & print the line # lines past the current line.
- * Make that the current line. */
-void move(int line)
+* Make that the current line. */
+void moveCurrentLine(int line)
 {
+	cout << "Move called!" << endl;
 }
 
 /* Saves the file to disk and quits the editor. */
